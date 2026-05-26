7c074ead7359c565dd29a31d32722674733c2412
jhoesch <jhoesch@student.tgm.ac.at>
2026-05-26 15:18:40 +0200

    test angepasst

--- src/hello.py
+++ src/hello.py
@@ -15,7 +15,7 @@ def hello_spencer():
     f.close()
 
     return jsonify({
-        "message": "Hello Spencer",
+        "message": "Hello sommer",
         "counter" : counter,
         "status": "success"
     })
